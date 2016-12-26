78ff9aca189ee2ec81d8094a95ad6b067fdb3805
Robert J. Hansen <rjh@sixdemonbag.org>
2016-12-26 12:56:53 -0500

    Documentation enhancement.

--- mkwin32.py
+++ mkwin32.py
@@ -8,6 +8,20 @@ import subprocess
 import re
 import sys
 
+# To use this with GitHub's Git Shell, add the following
+# to your Documents\WindowsPowerShell\profile.ps1 file.
+#
+# pushd 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools'    
+cmd /c "vsvars32.bat&set" |
+# foreach {
+#   if ($_ -match "=") {
+#     $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+#   }
+# }
+# popd
+# write-host "`nVisual Studio 2015 Command Prompt variables set." -ForegroundColor Yellow
+
+
 # User-serviceable parts here
 qtdir = r"C:\Qt\Qt5.7.1\5.7\msvc2015\bin\\"
 vc14dir = r"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\\"
