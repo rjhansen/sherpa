439bd32cc52a8d86028ac8e7432b4963f49d7d9c
Robert J. Hansen <rjh@sixdemonbag.org>
2017-01-13 09:40:55 -0500

    Forgot to remove ~ and _CMake*.

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf *.o moc_* *.depends ui_* cmake_install.cmake CMakeCache.txt CMakeFiles CPack* Makefile *.app Sherpa_automoc* qrc_sherpa.cpp
+rm -rf *.o moc_* *.depends ui_* cmake_install.cmake CMakeCache.txt CMakeFiles CPack* Makefile *.app Sherpa_automoc* qrc_sherpa.cpp *~ _CPack*
