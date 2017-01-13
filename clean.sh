bf9a779c488ddba54c045fb74938f541f2fe0eda
Robert J. Hansen <rjh@sixdemonbag.org>
2017-01-13 10:30:37 -0500

    Neglected to remove automoc files.

--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-rm -rf *.o moc_* *.depends ui_* cmake_install.cmake CMakeCache.txt CMakeFiles CPack* Makefile *.app Sherpa_automoc* qrc_sherpa.cpp *~ _CPack* Sherpa sherpa
+rm -rf *.o moc_* *.depends ui_* cmake_install.cmake CMakeCache.txt CMakeFiles CPack* Makefile *.app Sherpa_automoc* qrc_sherpa.cpp *~ _CPack* Sherpa sherpa *automoc.cpp
+
